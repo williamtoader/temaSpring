b53a5b73c32b2f35b2107b181f80e5d3bede6782
Wiliam Toader <wiliamtoader@protonmail.com>
2022-04-02 03:38:25 +0300

    Deleted duplicate column orderId

--- src/main/java/com/example/temaspring/model/OrderDetails.java
+++ src/main/java/com/example/temaspring/model/OrderDetails.java
@@ -16,7 +16,6 @@ import javax.persistence.*;
 public class OrderDetails {
     public OrderDetails(OrderDetailsDTO dto) {
         this.id = dto.getId();
-        this.orderId = dto.getOrderId();
         this.price = dto.getPrice();
         this.quantity = dto.getQuantity();
     }
@@ -25,8 +24,6 @@ public class OrderDetails {
     @NotNull
     @GeneratedValue
     private Long id;
-
-    private Long orderId;
 
     Double price;
     Integer quantity;
